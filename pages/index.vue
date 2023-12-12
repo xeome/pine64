b953a8200fdd42ae8be2d090793cf0d78acc6e66
Bekir Pehlivan <bekir.pehlivan@mono.net.tr>
2023-12-12 23:44:02 +0300

    added slides

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template></template>
+<template>
+    <TheNavBar />
+    <FixedBackground bgimage="laptop" title="PineBook Pro" />
+    <Carousel />
+    <FixedBackground bgimage="phone" title="PinePhone Pro" />
+    <Carousel />
+    <FixedBackground bgimage="tab" title="PineBook Pro" />
+    <Footer />
+</template>
