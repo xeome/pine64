750c5e19c4a00694038c5b7995aaef2c92b5d42e
Bekir Pehlivan <bekir.pehlivan@mono.net.tr>
2023-12-12 23:42:08 +0300

    fixed background

---
+++ components/FixedBackground.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="bgimage" >
+        <h2 class="text-5xl text-white mt-10">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FixedBackground',
+    props: [
+        'bgimage',
+        'title'
+    ]
+}
+</script>
+
+<style>
+.laptop{
+    background-attachment: fixed;
+    min-height: 600px;
+    background-image: url("/assets/PBP-placeholder.jpeg");
+}
+.phone{
+    background-attachment: fixed;
+    min-height: 600px;
+    background-image: url("/assets/pinephone-placeholder.jpeg");
+}
+.tab{
+    background-attachment: fixed;
+    min-height: 600px;
+    background-image: url("/assets/PineTabPlaceholder.jpeg");
+}
+</style>
